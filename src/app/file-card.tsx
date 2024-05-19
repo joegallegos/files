b0579cc3336466e51938ed78e3df9b1207a40eb9
Joe Gallegos <j.gallegos25u@gmail.com>
2024-05-18 22:35:37 -0600

    add cards and ability to delete

---
+++ src/app/file-card.tsx
@@ -0,0 +1,105 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import { MoreVertical, TrashIcon } from 'lucide-react';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog';
+import { useState } from 'react';
+import { useMutation } from 'convex/react';
+import { api } from '../../convex/_generated/api';
+import { Doc } from '../../convex/_generated/dataModel';
+import { useToast } from '@/components/ui/use-toast';
+
+function FileCardActions({ file }: { file: Doc<'files'> }) {
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const deleteFile = useMutation(api.files.deleteFile);
+  const { toast } = useToast();
+
+  return (
+    <>
+      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                await deleteFile({
+                  fileId: file._id,
+                });
+                toast({
+                  variant: 'default',
+                  title: 'File deleted Successfully',
+                  description: 'Your file is now deleted from your account',
+                });
+              }}
+            >
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() => setIsConfirmOpen(true)}
+            className="flex gap-1 text-red-500 items-center cursor-pointer"
+          >
+            <TrashIcon className="w-4 h-4" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export default function FileCard({ file }: { file: Doc<'files'> }) {
+  return (
+    <Card>
+      <CardHeader className="relative">
+        <CardTitle>{file.name}</CardTitle>
+        <div className="absolute top-2 right-2">
+          <FileCardActions file={file} />
+        </div>
+        {/* <CardDescription>Card Description</CardDescription> */}
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Download</Button>
+      </CardFooter>
+    </Card>
+  );
+}
